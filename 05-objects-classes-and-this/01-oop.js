de41c1af9679e7386f1edcb37009d571f5cb0aeb
Ana Toader <ana.toader@softwire.com>
2025-07-21 11:29:25 +0300

    Objects, classes and this exercises

---
+++ 05-objects-classes-and-this/01-oop.js
@@ -0,0 +1,13 @@
+// TODO: create the Person class using a function
+function Person(name, age) {
+    this.name = name
+    this.age = age
+    this.describe = function() {
+        return `${this.name}, ${this.age} years old`
+    }
+}
+
+var jack = new Person("Jack", 25);
+var jill = new Person("Jill", 24);
+console.log(jack.describe());
+console.log(jill.describe());
